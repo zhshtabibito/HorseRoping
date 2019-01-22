5fa6f4eb5535710e9a7724179e4bdbaa018e2920
zhshtabbito <zhshtabbito@foxmail.com>
2019-01-22 12:04:14 +0800

    first commit

---
+++ Assets/Scripts/Horse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horse : MonoBehaviour
+{
+    public int state; // 0=free, 1-4=player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public static int MAIN = 0;
+    public static int JIASHI = 1;
+    public int stateGame;
+    public float timeMain;
+
+    public Horse horse;
+    public List<Player> players;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeMain = 150f;
+        StartCoroutine("TimingEnd");
+    }
+
+    IEnumerator TimingEnd()
+    {
+        yield return new WaitForSeconds(timeMain);
+        Player winner = players[0];
+        foreach (Player player in players)
+        {
+            if (player.score > winner.score)
+            {
+                winner = player;
+            }
+        }
+        if(horse.state != winner.id)
+        {
+            stateGame = JIASHI;
+        }
+        else
+        {
+            GameOver(winner);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stateGame == JIASHI)
+        {
+
+        }
+    }
+
+    public void GameOver(Player winner)
+    {
+        Debug.Log("Game Over!");
+        Debug.Log("Palyer" + winner.id.ToString() + " wins!");
+        Time.timeScale = 0;
+    }
+}
