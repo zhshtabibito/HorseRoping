704a4563f90f10076f8cd2dacc04ee97be5c1808
zhshtabbito <zhshtabbito@foxmail.com>
2019-01-23 16:35:51 +0800

    complete-func

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,14 +23,17 @@ public class Player : MonoBehaviour
     public GameObject AimerObj;
     private Aimer aimer;
 
-    private bool canDash;
+    private bool canDash = true;
     private float cdDash = 8f;
     private float rateDash = 3f;
     private float timeDash = 1f;
 
-    private bool canPull;
+    private bool canPull = true;
     private float cdPull = 8f;
     // private float ratePull = 1 / 3;
+
+    private bool canRope = true;
+    private float cdRope = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -57,16 +60,17 @@ public class Player : MonoBehaviour
             float UD = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
             rb.MovePosition(rb.position + new Vector2(LR, UD) * spd * Time.deltaTime);
 
+            // charing and rope
             if (Input.GetKey(KeyCode.Space) && (state == FREE || state == CHARGING))
             {
                 state = CHARGING;
                 aimer.AddR();
             }
-            else if (Input.GetKeyUp(KeyCode.Space) && state == CHARGING)
+            else if (Input.GetKeyUp(KeyCode.Space) && state == CHARGING && canRope)
             {
                 StartCoroutine("RopeHorse");
             }
-
+            // skill dash n pull
             if (Input.GetKeyDown(KeyCode.LeftShift) && canDash && state==ROPING)
             {
                 // Dash
@@ -83,15 +87,43 @@ public class Player : MonoBehaviour
             }
 
         }
-
         //************************************************
+
+        // horse too far while roping
+        if (state == ROPING)
+        {
+            if ((World.horse.transform.position - transform.position).magnitude > aimer.R + lenRope)
+            {
+                state = FREE;
+                Debug.Log("Rope broken");
+                World.horse.state = 0;
+                World.HandleHorseState();
+                StartCoroutine("RopeCD");
+
+            }
+            else if((World.horse.transform.position - transform.position).magnitude > 0.8f*(aimer.R + lenRope))
+            {
+                // warning
+
+            }
+
+        }
+
 
     }
 
     public void BeDizzy()
     {
-        World.horse.state = 0;
+        aimer.HideAimer();
         state = DIZZY;
+        StartCoroutine("Dizzy");
+    }
+
+    IEnumerator Dizzy()
+    {
+        // anime
+        yield return new WaitForSeconds(2);
+        aimer.ResetAimer();
     }
 
     IEnumerator DashCD()
@@ -108,17 +140,58 @@ public class Player : MonoBehaviour
         canPull = true;
     }
 
+    IEnumerator RopeCD()
+    {
+        canRope = false;
+        yield return new WaitForSeconds(cdRope);
+        aimer.ResetAimer();
+        canRope = true;
+    }
+
     IEnumerator RopeHorse()
     {
         state = THROWING;
+        aimer.HideAimer();
+        // play anime
         yield return new WaitForSeconds(aimer.CalDelay());
-        if ((World.horse.transform.position - aimer.transform.position).magnitude <= aimer.R)
+        Player enemy = World.players[2 - id];
+        if ((enemy.transform.position - aimer.transform.position).magnitude <= aimer.R)
         {
-            state = ROPING;
-            // horse roped
+            // rope player
+            state = FREE;
+            Debug.Log("Player roped!");
+            enemy.BeDizzy();
+            World.horse.state = 0;
+            World.HandleHorseState();
+            StartCoroutine("RopeCD");
         }
-
-
+        else if ((World.horse.transform.position - aimer.transform.position).magnitude <= aimer.R)
+        {
+            // rope horse
+            if(World.horse.state == 0) // horse free
+            {
+                state = ROPING;
+                Debug.Log("Free Horse roped!");
+                World.horse.state = id;
+            }
+            else // horse roped
+            {
+                // Player enemy = World.players[World.horse.state - 1];
+                state = FREE;
+                Debug.Log("Roped Horse roped!");
+                enemy.state = 0;
+                enemy.aimer.ResetAimer();
+                World.horse.state = 0;
+                World.HandleHorseState();
+                StartCoroutine("RopeCD");
+            }
+        }
+        else // empty
+        {
+            Debug.Log("Nothing roped!");
+            state = FREE;
+            StartCoroutine("RopeCD");
+        }
+        
     }
-
 }
