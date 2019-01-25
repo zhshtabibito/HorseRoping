f8e70a2ad64d1f811cd09723cb4fdd807991c4f4
zhshtabbito <zhshtabbito@foxmail.com>
2019-01-25 15:14:49 +0800

    add-shadow-n-rope

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
 {
     private static int FREE = 0;
     private static int CHARGING = 1;
-    private static int ROPING = 2;
+    public static int ROPING = 2;
     private static int DIZZY = 3;
     private static int THROWING = 4;
 
@@ -23,6 +23,7 @@ public class Player : MonoBehaviour
 
     public GameObject AimerObj;
     public GameObject Rope;
+    public GameObject Rope_circle;
     private Aimer aimer;
 
     private bool canDash = true;
@@ -120,8 +121,8 @@ public class Player : MonoBehaviour
 
             // skill dash n pull
             if (((id==1)? // X dash
-                Input.GetKey(KeyCode.Joystick2Button1) :
-                Input.GetKey(KeyCode.Joystick4Button1)) &&
+                Input.GetKeyDown(KeyCode.Joystick2Button1) :
+                Input.GetKeyDown(KeyCode.Joystick4Button1)) &&
                 canDash && state==ROPING)
             {
                 DashCD();
@@ -129,8 +130,8 @@ public class Player : MonoBehaviour
                 spd *= rateDash;
             }
             else if (((id == 1) ? // Y pull
-                Input.GetKey(KeyCode.Joystick2Button3) :
-                Input.GetKey(KeyCode.Joystick4Button3)) &&
+                Input.GetKeyDown(KeyCode.Joystick2Button3) :
+                Input.GetKeyDown(KeyCode.Joystick4Button3)) &&
                 canPull && state == ROPING)
             {
                 World.horse.SetPulled(rb.position - World.horse.GetPosition(), spd * rateDash * 2);
@@ -149,6 +150,7 @@ public class Player : MonoBehaviour
             {
                 state = FREE;
                 Debug.Log("Rope broken");
+                GetComponentInChildren<Rope>().BreakLine();
                 World.horse.TryStruggle();
                 World.HandleHorseState();
                 StartCoroutine("RopeCD");
@@ -157,7 +159,7 @@ public class Player : MonoBehaviour
             else if((World.horse.transform.position - transform.position).magnitude > 0.8f*(aimer.R + lenRope))
             {
                 // warning
-
+                Debug.Log("Warning!");
             }
 
         }
@@ -220,8 +222,9 @@ public class Player : MonoBehaviour
         aimer.HideAimer();
         // play anime
         m_PlayerAudio.PlayThrow();
- 
+        Rope_circle.GetComponent<Rope>().Throw(aimer.CalDelay(), World.horse.transform.position - transform.position);
         yield return new WaitForSeconds(aimer.CalDelay());
+
         Player enemy = World.players[2 - id];
         if ((enemy.transform.position - aimer.transform.position).magnitude <= aimer.R)
         {
@@ -231,6 +234,7 @@ public class Player : MonoBehaviour
             enemy.BeDizzy();
             enemy.m_Animator.SetBool(m_HashDizzyPara, true);
             enemy.m_PlayerAudio.PlayDize();
+            enemy.GetComponentInChildren<Rope>().BreakLine();
             World.horse.state = 0;
             World.HandleHorseState();
             StartCoroutine("RopeCD");
@@ -251,6 +255,7 @@ public class Player : MonoBehaviour
                 Debug.Log("Roped Horse roped!");
                 enemy.state = 0;
                 enemy.aimer.ResetAimer();
+                enemy.GetComponentInChildren<Rope>().BreakLine();
                 World.horse.TryStruggle();
                 World.HandleHorseState();
                 StartCoroutine("RopeCD");
--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -2,17 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rops : MonoBehaviour
+public class Rope : MonoBehaviour
 {
+    private bool isThrowing;
+    private SpriteRenderer sr;
+    public Vector3 vec;
+    public float time;
+    public GameObject line;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        sr.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (isThrowing)
+        {
+            transform.position = transform.position + vec * Time.deltaTime / time;
+            line.GetComponent<LineRenderer>().SetPosition(0, GetComponentInParent<Player>().transform.position);
+            line.GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        }
+        else if(GetComponentInParent<Player>().state == Player.ROPING)
+        {
+            line.GetComponent<LineRenderer>().SetPosition(0, GetComponentInParent<Player>().transform.position);
+            line.GetComponent<LineRenderer>().SetPosition(0, World.horse.transform.position);
+        }
     }
+
+    public void Throw(float time, Vector3 vec)
+    {
+        this.time = time;
+        this.vec = vec;
+        transform.position = GetComponentInParent<Player>().transform.position;
+        line.GetComponent<LineRenderer>().enabled = true;
+        line.GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        line.GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        StartCoroutine("TimingAndThrow");
+    }
+
+    IEnumerator TimingAndThrow()
+    {
+        isThrowing = true;
+        sr.enabled = true;
+        yield return new WaitForSeconds(time);
+        isThrowing = false;
+        sr.enabled = false;
+
+    }
+
+    public void BreakLine()
+    {
+        line.GetComponent<LineRenderer>().enabled = false;
+    }
+
+
+
 }
