dbd4ec5d4a24e30170664f79b31a151fc06349f1
Hotkang <353223003@qq.com>
2019-01-25 10:49:16 +0800

    general commit

---
+++ Assets/UI/mymenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mymenu : MonoBehaviour
+{
+    public RectTransform PauseTr;
+     ///public static GameState gameState = UIGameState.Menu;
+    public void Pause()
+    {
+        
+        PauseTr.gameObject.SetActive(true);
+    }
+    public void mainmenu()
+    {
+        
+    
+       
+        PauseTr.gameObject.SetActive(false);
+        
+    }
+    
+   public void restartbtn()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    public void restartbtn2(){
+        SceneManager.LoadScene(0);
+        ///gameState = UIGameState.Menu;
+        //restart.gameObject.SetActive(false);
+    }
+    public void quit(){
+        Application.Quit();
+    }
+    
+    void Start()
+    {
+        
+    }
+
+}
